1df7bab76e459d841e3eafa137c6b11764b2a85c
motazhaj <motazhajm@gmail.com>
2023-09-08 19:16:53 +0300

    Add app.js file with base variables defined

---
+++ app.js
@@ -0,0 +1,8 @@
+const startGameButton = document.getElementById("start-game");
+const showNamesOverlayButton = document.getElementById("show-names-overlay");
+const cancelSetNamesButton = document.getElementById("cancel-button");
+const setNamesButton = document.getElementById("set-names-button");
+
+
+const player1Name = document.getElementById("player1Name");
+const player2Name = document.getElementById("player2Name");
