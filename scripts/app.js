6b1f1f26b2b12fda932c99a5784e4d3c55cd001c
motazhaj <motazhajm@gmail.com>
2023-09-09 21:41:51 +0300

    Move script file into scripts folder for better project structure

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,12 @@ const startGameButton = document.getElementById("start-game");
 const showNamesOverlayButton = document.getElementById("show-names-overlay");
 const cancelSetNamesButton = document.getElementById("cancel-button");
 const setNamesButton = document.getElementById("set-names-button");
-
+const namesOverlay = document.getElementById("set-names-overlay");
 
 const player1Name = document.getElementById("player1Name");
 const player2Name = document.getElementById("player2Name");
+
+showNamesOverlayButton.addEventListener(
+  "click", () =>
+  namesOverlay.style.display = "block"
+);
