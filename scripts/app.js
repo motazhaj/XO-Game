4f871256bbf5a0cc7779a6e71e5a0cdb09e8add2
motazhaj <motazhajm@gmail.com>
2023-09-10 15:05:30 +0300

    Implement set names form

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,15 @@ const startGameButton = document.getElementById("start-game");
 const showNamesOverlayButton = document.getElementById("show-names-overlay");
 const namesOverlay = document.getElementById("set-names-overlay");
 const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
 
 const player1NameInput = document.getElementById("player1NameLabel");
 const player2NameInput = document.getElementById("player2NameLabel");
 const player1Name = document.getElementById("player1Name");
 const player2Name = document.getElementById("player2Name");
-const setNamesButton = document.getElementById("set-names-button");
 const cancelSetNamesButton = document.getElementById("cancel-button");
 
 showNamesOverlayButton.addEventListener("click", openNamesOverlay);
 cancelSetNamesButton.addEventListener("click", closeNamesOverlay);
 backdropElement.addEventListener("click", closeNamesOverlay);
-setNamesButton.addEventListener("click", setNames);
+formElement.addEventListener("submit", setNames);
--- scripts/config.js
+++ scripts/config.js
@@ -8,8 +8,20 @@ function closeNamesOverlay() {
   namesOverlay.style.display = "none";
 }
 
-function setNames() {
-  player1Name.innerHTML = "test";
-  player2Name.innerHTML = "test2";
+function setNames(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  let enteredPlayer1Name = formData.get("player1Name").trim();
+  let enteredPlayer2Name = formData.get("player2Name").trim();
+
+  if (!enteredPlayer1Name) {
+    enteredPlayer1Name = "Player 1";
+  }
+  if (!enteredPlayer2Name) {
+    enteredPlayer2Name = "Player 2";
+  }
+
+  player1Name.innerHTML = enteredPlayer1Name;
+  player2Name.innerHTML = enteredPlayer2Name;
   closeNamesOverlay();
 }
