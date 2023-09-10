4493a983987fbcc274848c115ddf68bc10917104
motazhaj <motazhajm@gmail.com>
2023-09-10 13:46:41 +0300

    Conform to start of JS code implementation

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,16 @@
 const startGameButton = document.getElementById("start-game");
 const showNamesOverlayButton = document.getElementById("show-names-overlay");
-const cancelSetNamesButton = document.getElementById("cancel-button");
-const setNamesButton = document.getElementById("set-names-button");
 const namesOverlay = document.getElementById("set-names-overlay");
 
+const player1NameInput = document.getElementById("player1NameLabel");
+const player2NameInput = document.getElementById("player2NameLabel");
 const player1Name = document.getElementById("player1Name");
 const player2Name = document.getElementById("player2Name");
+const setNamesButton = document.getElementById("set-names-button");
+const cancelSetNamesButton = document.getElementById("cancel-button");
 
-showNamesOverlayButton.addEventListener(
-  "click", () =>
-  namesOverlay.style.display = "block"
-);
+showNamesOverlayButton.addEventListener("click", openNamesOverlay);
+
+cancelSetNamesButton.addEventListener("click", closeNamesOverlay);
+
+setNamesButton.addEventListener("click", setNames);
---
+++ scripts/config.js
@@ -0,0 +1,12 @@
+function openNamesOverlay() {
+  namesOverlay.style.display = "block";
+}
+
+function closeNamesOverlay() {
+  namesOverlay.style.display = "none";
+}
+
+function setNames() {
+  player1Name.innerHTML = "test";
+  player2Name.innerHTML = "test2";
+}
