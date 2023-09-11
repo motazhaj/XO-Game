128c79c9dd6c7e5165455981fd5ced413d9c16ff
motazhaj <motazhajm@gmail.com>
2023-09-11 12:19:27 +0300

    Add game over logic

--- scripts/game.js
+++ scripts/game.js
@@ -77,8 +77,57 @@ function checkGameOver() {
       return gameData[0][0];
     }
   }
-  //Diagonal: Top right -> bottom left
 
+  //Diagonal: Top right -> bottom left
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][2] > 0 &&
+      gameData[0][2] === gameData[1][1] &&
+      gameData[1][1] === gameData[2][0]
+    ) {
+      console.log("Game Over you won " + gameData[0][2]);
+      return gameData[0][2];
+    }
+  }
+}
+
+function checkGameOver() {
+  //Horizontal
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      console.log("Game Over you won " + gameData[i][0]);
+      return gameData[i][0];
+    }
+  }
+
+  //Vertical
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[1][i] === gameData[2][i]
+    ) {
+      console.log("Game Over you won " + gameData[0][i]);
+      return gameData[0][i];
+    }
+  }
+  //Diagonal: Top left -> bottom right
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][0] > 0 &&
+      gameData[0][0] === gameData[1][1] &&
+      gameData[1][1] === gameData[2][2]
+    ) {
+      console.log("Game Over you won " + gameData[0][0]);
+      return gameData[0][0];
+    }
+  }
+
+  //Diagonal: Top right -> bottom left
   for (let i = 0; i < 3; i++) {
     if (
       gameData[0][2] > 0 &&
