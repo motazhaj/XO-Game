a21045b177dcf6d58cbf1ee61c8fc9a4499ca986
motazhaj <motazhajm@gmail.com>
2023-09-11 12:18:59 +0300

    Add game data logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,11 @@
+let activePlayer = 0;
+
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
 const startGameButton = document.getElementById("start-game");
 const showNamesOverlayButton = document.getElementById("show-names-overlay");
 const namesOverlay = document.getElementById("set-names-overlay");
@@ -10,13 +18,13 @@ const player1Name = document.getElementById("player1Name");
 const player2Name = document.getElementById("player2Name");
 const cancelSetNamesButton = document.getElementById("cancel-button");
 
-let activePlayer = 0;
 const activePlayerElement = document.getElementById("active-player");
-const gameGridElements = document.getElementById("game-board");
+const gameGridElement = document.getElementById("game-board");
 
 showNamesOverlayButton.addEventListener("click", openNamesOverlay);
 cancelSetNamesButton.addEventListener("click", closeNamesOverlay);
 backdropElement.addEventListener("click", closeNamesOverlay);
 formElement.addEventListener("submit", setNames);
 
-gameGridElements.addEventListener("click", selectGameGrid);
+startGameButton.addEventListener("click", startNewGame);
+gameGridElement.addEventListener("click", selectGameGrid);
--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,27 @@
 function selectGameGrid(event) {
-  if (event.target.tagName !== "LI") {
+  const selectedBox = event.target;
+  if (selectedBox.tagName !== "LI") {
     return;
   }
 
-  if (activePlayer === 0) {
-    event.target.textContent = "X";
-  } else {
-    event.target.textContent = "O";
+  const selectedColumn = selectedBox.dataset.col - 1;
+  const selectedRow = selectedBox.dataset.row - 1;
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    return;
   }
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+  if (activePlayer === 0) {
+    selectedBox.textContent = "X";
+  } else {
+    selectedBox.textContent = "O";
+  }
+
   switchPlayer();
-  event.target.classList.add("disabled");
+  selectedBox.classList.add("disabled");
+  checkGameOver();
 }
 
 function switchPlayer() {
@@ -19,5 +31,62 @@ function switchPlayer() {
   } else {
     activePlayer = 1;
     activePlayerElement.innerHTML = player2Name.innerHTML;
+  }
+}
+
+function startNewGame() {
+  const selectedElements = document.querySelectorAll("#game-board li");
+  for (const selectedElement of selectedElements) {
+    selectedElement.innerHTML = "";
+    selectedElement.classList.remove("disabled");
+  }
+}
+
+function checkGameOver() {
+  //Horizontal
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      console.log("Game Over you won " + gameData[i][0]);
+      return gameData[i][0];
+    }
+  }
+
+  //Vertical
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[1][i] === gameData[2][i]
+    ) {
+      console.log("Game Over you won " + gameData[0][i]);
+      return gameData[0][i];
+    }
+  }
+  //Diagonal: Top left -> bottom right
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][0] > 0 &&
+      gameData[0][0] === gameData[1][1] &&
+      gameData[1][1] === gameData[2][2]
+    ) {
+      console.log("Game Over you won " + gameData[0][0]);
+      return gameData[0][0];
+    }
+  }
+  //Diagonal: Top right -> bottom left
+
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][2] > 0 &&
+      gameData[0][2] === gameData[1][1] &&
+      gameData[1][1] === gameData[2][0]
+    ) {
+      console.log("Game Over you won " + gameData[0][2]);
+      return gameData[0][2];
+    }
   }
 }
